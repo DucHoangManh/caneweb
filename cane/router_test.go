4ea0a256bff96f5a31924b5110ab04172a944f3e
duchoang <duchoang@beeketing.net>
2022-01-02 18:19:03 +0700

    router test

--- cane/router_test.go
+++ cane/router_test.go
@@ -1,19 +1,30 @@
 package cane
 
 import (
-	"fmt"
 	"testing"
 )
 
-type s struct {
-	Name string
+func newTestRouter() *router {
+	r := newRouter()
+	r.addRoute("POST", "/test", HandleFunc(sampleHandler))
+	r.addRoute("GET", "/post", HandleFunc(sampleHandler))
+	return r
 }
 
-func Test(t *testing.T) {
-	sl := make([]*s, 0)
-	sl = append(sl, &s{Name: "er"})
-	sl = append(sl, &s{Name: "ad"})
-	for _, v := range sl {
-		fmt.Println(v.Name)
+func TestRouter(t *testing.T) {
+	router := newTestRouter()
+	if _, ok := router.handlers["POST-/test"]; !ok {
+		t.Error("err route configuration")
 	}
+	if _, ok := router.handlers["GET-/post"]; !ok {
+		t.Error("err route configuration")
+	}
+	if _, ok := router.handlers["GET-/test"]; ok {
+		t.Error("err route configuration")
+	}
+
+}
+
+func sampleHandler(c *Ctx) {
+	c.String(200, "%s", "test")
 }
